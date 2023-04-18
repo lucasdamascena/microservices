57394722089a09931ecd2a786be754032634fce3
Lucas Damascena <damascena.lucasg@gmail.com>
2023-04-17 23:06:24 -0300

    IEEstudo de Resilience4j

---
+++ 04-book-service/src/main/java/br/com/apollo/controllers/FooBarController.java
@@ -0,0 +1,35 @@
+package br.com.apollo.controllers;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
+
+    @GetMapping("/foo-bar")
+    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default")
+    public String fooBar() {
+        logger.info("Request to foo-bar is received!");
+
+        // var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+
+        return "Foo-Bar!!!";
+    }
+
+    public String fallbackMethod(Exception ex) {
+        return "fallbackMethod foo-bar!!!";
+    }
+}
+
+
+
